ac595e17a9109d0db4c4f773e1aa0e770276ca38
shreeyantrapc20 <shreeyantrapc@gmail.com>
2024-07-16 07:07:19 +0545

    Updated

--- 18 PHPFormValidation/index.php
+++ 18 PHPFormValidation/index.php
@@ -7,18 +7,49 @@
 </head>
 <body>
     <?php 
+        $input = "";
+        $nameerr="";
+
         if ($_SERVER["REQUEST_METHOD"]=="POST") {
             
             if (empty($_POST["uname"])) {
-                echo "<script> alert('Enter the username');</script>";
-            }
+
+                $nameerr = "Provide the input";
+                
+            } else {
+
+                $input = $_POST["uname"];
+                if(!ValidateInputLength($input, 2, 10)) {
+                        $nameerr = "Invalid input length";
+                } else if(!preg_match("/^[a-zA-Z]*$/", $input)) {
+                        $nameerr = "Only alphabets are allowed";
+                } 
+
+            }  
+            
 
         }
     
+    
+    
+    function ValidateInputLength($input, $minLength, $maxLength) {
+
+        $length = strlen($input);
+
+        if($length < $minLength || $length > $maxLength) {
+           return false; 
+        }
+
+        return true;
+    }
+
     ?>
 
+
+
     <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-        <input type="text" name="uname" id="uname" placeholder="Username">
+        <span class="err"><?php echo $nameerr;?></span> <br><br>
+        <input type="text" name="uname" id="uname" placeholder="Username" value="<?php echo $input; ?>"> <br><br>
         <input type="submit" value="Submit">
     </form>
 </body>
