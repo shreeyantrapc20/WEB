21dd465fe2ab25b08425baa82b7ebe29525636cb
shreeyantrapc20 <shreeyantrapc@gmail.com>
2024-07-17 07:05:52 +0545

    Updated

--- 18 PHPFormValidation/index.php
+++ 18 PHPFormValidation/index.php
@@ -7,18 +7,29 @@
 </head>
 <body>
     <?php 
+    //defiining variables
         $input = "";
         $nameerr="";
+        $emailerr="";
+        $ageerr="";
+        $emailIP = "";
+        $age="";
+        $gender="";
+        $gendererr="";
 
         if ($_SERVER["REQUEST_METHOD"]=="POST") {
             
-            if (empty($_POST["uname"])) {
+            //getting input
+            $input = $_POST["uname"];
+            $emailIP = $_POST["uemail"];
+            $age = $_POST["uage"];
+            $gender = $_POST["gender"];
 
-                $nameerr = "Provide the input";
-                
+            //username validation
+            if (empty($input)) {
+                $nameerr = "Provide the input";    
             } else {
 
-                $input = $_POST["uname"];
                 if(!ValidateInputLength($input, 2, 10)) {
                         $nameerr = "Invalid input length";
                 } else if(!preg_match("/^[a-zA-Z]*$/", $input)) {
@@ -26,6 +37,28 @@
                 } 
 
             }  
+
+            //email validation
+            
+            if(empty($emailIP)) {
+                $emailerr = "Provide the email input";
+            }
+            else if(!filter_var($emailIP, FILTER_VALIDATE_EMAIL)) {
+                $emailerr = "Invalid email";
+            }
+
+            //age validation
+           
+            if(empty($age)) {
+                $ageerr = "Provide the input";
+            } else if($age <= 10 || $age >= 50) {
+                $ageerr="Age must be between 10 and 50";
+            }
+
+            //gender validation
+            if(empty($gender)) {
+                $gendererr = "What is your gender?";
+            }
             
 
         }
@@ -48,9 +81,15 @@
 
 
     <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-        <span class="err"><?php echo $nameerr;?></span> <br><br>
+        <span class="err"><?php echo $nameerr;?></span> 
         <input type="text" name="uname" id="uname" placeholder="Username" value="<?php echo $input; ?>"> <br><br>
-        <input type="submit" value="Submit">
+        <span class="err"><?php echo $emailerr;?></span> 
+        <input type="text" name="uemail" id="uemail" placeholder="Email" value="<?php echo $emailIP; ?>"> <br><br>
+        <span class="err"><?php echo $ageerr;?></span> 
+        <input type="text" name="uage" id="uage" placeholder="Age" value="<?php echo $age; ?>"> <br><br>
+        <span class="err"><?php echo $gendererr;?></span> 
+        Gender: Male <input type="radio" name="gender" id="gender" value="male"> Female <input type="radio" name="gender" id="gender" value="female">
+        <br><input type="submit" value="Submit">
     </form>
 </body>
 </html>
