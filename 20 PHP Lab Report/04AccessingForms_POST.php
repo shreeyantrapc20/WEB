8e1d069ac2e3fbec84fa761cbc93929c122646a6
Binayak Niraula <binayak0128@gmail.com>
2024-07-19 16:11:26 +0545

    Updated

---
+++ 20 PHP Lab Report/04AccessingForms_POST.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="process.php" method = "POST">
+        <input type="number" name="num1" placeholder = "Enter number 1"><br><br>
+        <input type="number" name="num2" placeholder = "Enter number 2"><br><br>
+        <input type="submit" value="Compute">
+    </form>
+</body>
+</html>
